3c74e13339168a5388477225ede37a6471e12677
ShanTheNaan <slalani@vols.utk.edu>
2019-02-26 20:37:51 -0500

    Lots of changes

--- server.py
+++ server.py
@@ -19,65 +19,94 @@ def create_dir_html(dir_name):
     for file in os.listdir(dir_name):
         filename = os.fsdecode(file)
         if (os.path.isdir(dir_name+'/'+filename)):
-            print ("here")
             filename += '/'
         f += "<a href = \"" + filename+"\">"+filename+"</a><br>\n"
     f += "</body>\n</html>"
     return f.encode('utf-8')
 
+
 HOST, PORT = '',6969
-
-#prepare the socket and have it begin to listen
-listen_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-listen_socket.bind((HOST,PORT))
-listen_socket.listen(10)
-print ('Serving piping hot HTTP on port %s' % PORT)
-
-while True:
-    #Take in each request, print it to show http request
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024).decode('utf-8')
-    print (request)
-    print(request.split(' '))
-    if (request):
-        rqfile = request.split(' ')[1]
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    s.bind ((HOST, PORT))
     
-    if(rqfile == '/'): 
-        #default to index.html if there is no path
-        rqfile = '/index.html'
-    #rqfile = sys.path[0]+rqfile
-    try:
-        #open file, then tell what kind of file you're reading
-        if(rqfile == '/fileupload?'):
-            #this happens when a file is uploaded, write file to server side uploads folder
-            print('Time to write a file!')
+    s.listen (10)
+    print ('Serving piping hot HTTP on port %s' % PORT)
+    
+    while True:
+        conn, addr = s.accept()
+        data = conn.recv(1024)
+        if not data:
+            continue
+        
+        data = data.decode('utf-8')
+        print(data)
+        
+        data = data.split(' ')
+        rqfile = data[1]
+        
+        if (data[0] == "POST"): 
+            try:
+                first = True
+                while True:
+                    conn.settimeout(1)
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    
+                    if (first):
+                        data = data.strip().split(b'\r\n\r\n')
+                        fileName = data[0].split(b'\r\n')[-2].decode('utf-8').split("\"")[-2]
+                        boundry = data[0].decode('utf-8').split('\r\n')[0]
+                        fileData = data[1]
+                        first = False
+                    else:
+                        fileData += data
+
+
+            except socket.timeout:
+                boundry = ("\r\n" + boundry).encode('utf-8')
+                fileData = fileData.split(boundry)[0]
+
+                f = open("uploads/"+fileName, "wb")
+                f.write(fileData)
+                f.close()
+
+                header = 'HTTP/1.1 200 OK\n\n'
+                http_response = create_dir_html(os.getcwd()+'/uploads')
+
+
         else:
-            rqfile = sys.path[0]+rqfile
-            print (rqfile)
-            f = open(rqfile,'rb')
-            http_response = f.read()
-            f.close()
-            header = 'HTTP/1.1 200 OK\n'
-            #prepare mime type based on file, default to html
-            if(rqfile.endswith('.jpg')):
-                mime  = "image/jpg"
-            elif(rqfile.endswith('.png')):
-                mime = 'image/png'
-            else:
-                mime = "text/html"
-            header += 'Content-Type: '+str(mime)+'\n\n'
-    except IsADirectoryError:
-        #create custom response for directory
-        header = 'HTTP/1.1 200 OK\n\n'
-        http_response = create_dir_html(rqfile)
-    except Exception:
-        #default to 404 for invalid requests
-        header = 'HTTP/1.1 404 Not Found\n\n'
-        http_response = '<html><body><center><h3>Error 404: File not found</h3><p>Go home user, you\'re drunk.</p></center></body></html>'.encode('utf-8')
- 
-    #combine header and http_response to send to the client
-    response = header.encode('utf-8')
-    response += http_response
-    client_connection.send(response)
-    client_connection.close()
+            if (rqfile == '/'):
+                rqfile = '/index.html'
+
+            try:
+                rqfile = sys.path[0] + rqfile
+                if (os.path.isdir(rqfile)):
+                    header = 'HTTP/1.1 200 OK\n\n'
+                    http_response = create_dir_html(rqfile)
+                else:
+                    f = open(rqfile,'rb')
+                    http_response = f.read()
+                    f.close()
+                
+                    header = 'HTTP/1.1 200 OK\n'
+                    if(rqfile.endswith('.jpg')):
+                        mime  = "image/jpg"
+                    elif(rqfile.endswith('.png')):
+                        mime = 'image/png'
+                    else:
+                        mime = "text/html"
+                    
+                    header += 'Content-Type: '+str(mime)+'\n\n'
+            
+            except Exception:
+                #default to 404 for invalid requests
+                header = 'HTTP/1.1 404 Not Found\n\n'
+                http_response = """<html><body><center><h3>Error 404: File not found</h3>
+                <p>Go home user, you\'re drunk.</p></center></body></html>"""
+                http_response = http_response.encode('utf-8')
+    
+        response = header.encode('utf-8')
+        response += http_response
+        conn.send(response)
